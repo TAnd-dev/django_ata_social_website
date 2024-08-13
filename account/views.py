fd593b7dd4dbf54d32628b391cde510204e17c81
Andrei <titov680andrei@gmail.com>
2024-08-13 19:09:57 +0400

    Added profile and ability to edit your data

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from account.forms import LoginForm, RegistrationForm
+from account.forms import LoginForm, RegistrationForm, UserEditForm, ProfileEditForm
 
 
 def user_login(request):
@@ -41,6 +41,21 @@ def register(request):
         form = RegistrationForm()
     return render(request, 'account/register.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
